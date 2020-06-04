4d04a59de5b34757fcbe9351c70e48c20e8fc9ed
eduardo c neto <eddycn3@gmail.com>
2020-06-04 13:34:01 -0300

    register_page design  done

--- lib/src/ui/register_form.dart
+++ lib/src/ui/register_form.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class RegisterForm extends StatefulWidget {
@@ -5,112 +6,190 @@ class RegisterForm extends StatefulWidget {
   _RegisterFormState createState() => _RegisterFormState();
 }
 
-//7A9968
-/*
- * 
- {
-    "user": {
-        "user_name": "guilherme.mantrovani@maxavaliators.com",
-        "password": "123456",
-        "user_type": 1
-    },
-    "user_info": {
-        "nome": "Guilherme Mantrovani",
-        "empresa": "Max Avaliators",
-        "site": "",
-        "email": "guilherme.mantrovani@maxavaliators.com",
-        "telefone": "019992534334"
-    }
-}
- */
 class _RegisterFormState extends State<RegisterForm> {
+  int currentFormIndex = 1;
+  CarouselController _carouselController = CarouselController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
         child: Form(
-          child: Column(
-            children: <Widget>[
-              Icon(Icons.assignment, size: 80, color: Colors.green[300]),
-              Padding(
-                padding: const EdgeInsets.only(left: 15, right: 15, top: 20),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(30, 20, 30, 0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.center,
+                  child: Icon(
+                    Icons.assignment,
+                    size: 70,
+                    color: Colors.green[200],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.center,
+                  child: Text(
+                    "Escolha um email e uma senha",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.black54,
+                      fontFamily: "SourceSansPro",
+                      letterSpacing: 2,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                CarouselSlider(
+                  carouselController: _carouselController,
+                  items: <Widget>[
+                    FirstFormStep(),
+                    SecondFormStep(),
+                  ],
+                  options: CarouselOptions(
+                    onPageChanged: (value, reason) {
+                      setState(() {
+                        currentFormIndex = value + 1;
+                      });
+                    },
+                    height: 300,
+                    viewportFraction: 1,
+                    initialPage: 0,
+                    reverse: false,
+                    enlargeCenterPage: true,
+                    scrollDirection: Axis.horizontal,
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Row(
                   children: <Widget>[
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      cursorColor: Colors.green,
-                      decoration: InputDecoration(
-                        hintText: "email",
-                        hintStyle: TextStyle(color: Colors.grey),
-                        contentPadding: EdgeInsets.symmetric(vertical: 15),
-                      ),
-                    ),
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      cursorColor: Colors.green,
-                      decoration: InputDecoration(
-                        hintText: "senha",
-                        hintStyle: TextStyle(color: Colors.grey),
-                        contentPadding: EdgeInsets.symmetric(vertical: 15),
-                      ),
-                    ),
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      cursorColor: Colors.green,
-                      decoration: InputDecoration(
-                        hintText: "nome",
-                        hintStyle: TextStyle(color: Colors.grey),
-                        contentPadding: EdgeInsets.symmetric(vertical: 15),
-                      ),
-                    ),
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      cursorColor: Colors.green,
-                      decoration: InputDecoration(
-                        hintText: "empresa",
-                        hintStyle: TextStyle(color: Colors.grey),
-                        contentPadding: EdgeInsets.symmetric(vertical: 15),
-                      ),
-                    ),
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      cursorColor: Colors.green,
-                      decoration: InputDecoration(
-                        hintText: "site",
-                        hintStyle: TextStyle(color: Colors.grey),
-                        contentPadding: EdgeInsets.symmetric(vertical: 15),
-                      ),
-                    ),
-                    TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      cursorColor: Colors.green,
-                      decoration: InputDecoration(
-                        hintText: "telefone",
-                        hintStyle: TextStyle(color: Colors.grey),
-                        contentPadding: EdgeInsets.symmetric(vertical: 15),
+                    Align(
+                      alignment: Alignment.bottomRight,
+                      child: RaisedButton.icon(
+                        color: Colors.green,
+                        splashColor: Colors.green[300],
+                        onPressed: currentFormIndex == 1
+                            ? () {
+                                _carouselController.jumpToPage(1);
+                              }
+                            : null,
+                        icon: Icon(
+                          Icons.skip_next,
+                          color: Colors.white,
+                        ),
+                        label: Text(
+                          "Próximo",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                              letterSpacing: 3,
+                              fontFamily: "SourceSansPro",
+                              fontWeight: FontWeight.bold),
+                        ),
                       ),
                     ),
                   ],
                 ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              FlatButton(
-                  color: Colors.green[300],
-                  onPressed: () {},
-                  child: Text(
-                    "Registrar",
-                    style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w800,
-                        fontSize: 20.0),
-                  )),
-            ],
+                SizedBox(height: 60),
+                Text(
+                  "$currentFormIndex / 2",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 30,
+                      fontFamily: "Pacifico"),
+                ),
+              ],
+            ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class FirstFormStep extends StatelessWidget {
+  const FirstFormStep({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          SizedBox(height: 20),
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            cursorColor: Colors.green,
+            decoration: InputDecoration(
+              hintText: "E-MAIL",
+              hintStyle: TextStyle(color: Colors.grey),
+            ),
+          ),
+          SizedBox(height: 20),
+          TextFormField(
+            keyboardType: TextInputType.visiblePassword,
+            cursorColor: Colors.green,
+            decoration: InputDecoration(
+              hintText: "SENHA",
+              hintStyle: TextStyle(color: Colors.grey),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SecondFormStep extends StatelessWidget {
+  const SecondFormStep({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            cursorColor: Colors.green,
+            decoration: InputDecoration(
+              hintText: "NOME COMPLETO",
+              hintStyle: TextStyle(color: Colors.grey),
+            ),
+          ),
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            cursorColor: Colors.green,
+            decoration: InputDecoration(
+              hintText: "EMPRESA",
+              hintStyle: TextStyle(color: Colors.grey),
+            ),
+          ),
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            cursorColor: Colors.green,
+            decoration: InputDecoration(
+              hintText: "SITE",
+              hintStyle: TextStyle(color: Colors.grey),
+            ),
+          ),
+          TextFormField(
+            keyboardType: TextInputType.emailAddress,
+            cursorColor: Colors.green,
+            decoration: InputDecoration(
+              hintText: "TELEFONE",
+              hintStyle: TextStyle(color: Colors.grey),
+            ),
+          ),
+        ],
       ),
     );
   }
